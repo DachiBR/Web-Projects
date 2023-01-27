1d3e492fcdd575d7cda3202c0f3a8f3b8e3a73b6
DachMan <75274431+DachiBR@users.noreply.github.com>
2023-01-27 22:28:41 +0400

    Add files via upload

---
+++ Project N5/script.js
@@ -0,0 +1,35 @@
+function change128(){
+    let memory = document.getElementById("phonememory");
+    memory.innerHTML = "128GB"
+}
+function change256(){
+    let memory = document.getElementById("phonememory");
+    memory.innerHTML = "256GB"
+}
+function change512(){
+    let memory = document.getElementById("phonememory");
+    memory.innerHTML = "512GB"
+}
+
+
+function changecolorgray(){
+    let memory = document.getElementById("phonecolor");
+    memory.innerHTML = "Gray"
+
+    let photo = document.getElementById("iphonephoto");
+    photo.src ="./img/gray.jpg"
+}
+function changecolorblue(){
+    let memory = document.getElementById("phonecolor");
+    memory.innerHTML = "Blue"
+
+    let photo = document.getElementById("iphonephoto");
+    photo.src ="./img/blue.jpg"
+}
+function changecolorgreen(){
+    let memory = document.getElementById("phonecolor");
+    memory.innerHTML = "Green"
+
+    let photo = document.getElementById("iphonephoto");
+    photo.src ="./img/green.jpg"
+}
